ada326dc60e6f2cb103f82066c17aa4bcb75d9fd
Frankyn Vieira <frankfhurt@gmail.com>
2020-09-21 01:35:29 -0300

    Fixed pom.xml and UsersCreateRestTests

--- src/test/java/com/techmahindra/customers/api/users/create/UsersCreateRestTests.java
+++ src/test/java/com/techmahindra/customers/api/users/create/UsersCreateRestTests.java
@@ -2,9 +2,10 @@ package com.techmahindra.customers.api.users.create;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techmahindra.customers.CustomersApplication;
-import com.techmahindra.customers.api.customers.repository.CustomerRepository;
 import com.techmahindra.customers.api.users.common.UserDto;
 import com.techmahindra.customers.api.users.common.UserTestDataBuilder;
+import com.techmahindra.customers.api.users.repository.User;
+import com.techmahindra.customers.api.users.repository.UserRepository;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,9 +15,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CustomersApplication.class)
@@ -30,19 +37,29 @@ public class UsersCreateRestTests {
     private ObjectMapper mapper;
 
     @Autowired
-    private CustomerRepository repository;
+    private UserRepository repository;
 
     @Test
     public void givenValidUser_whenCreate_thenSuccess() throws Exception {
         UserDto validUserDto = UserTestDataBuilder.createValidUserDto(null);
         String request = mapper.writeValueAsString(validUserDto);
 
-        mvc.perform(post("/users")
+
+        MvcResult mvcResult = mvc.perform(post("/users")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(request))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id", Matchers.is(1)));
+                .andReturn();
+
+        Optional<User> byUsernameOrEmail = repository.findByUsernameOrEmail(validUserDto.getName(), validUserDto.getEmail());
+        assertTrue(byUsernameOrEmail.isPresent());
+
+        User user = byUsernameOrEmail.get();
+        String response = mvcResult.getResponse().getContentAsString();
+        UserDto userDto = mapper.readValue(response, UserDto.class);
+
+        assertEquals(user.getId(), userDto.getId());
     }
 
     @Test
